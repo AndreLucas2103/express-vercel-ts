664b976852c3e69830652b68fe7876febbe89a6c
André Cabral <63832522+AndreLucas2103@users.noreply.github.com>
2022-11-18 12:25:43 +0000

    1

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-import { router } from "./api/product";
-
-
-app.use("/", router);
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
