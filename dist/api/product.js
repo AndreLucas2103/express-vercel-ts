b6e9565f8e34e60fc506caedb30977c006c62cd1
André Cabral <63832522+AndreLucas2103@users.noreply.github.com>
2022-11-18 13:33:36 +0000

    a

--- dist/api/product.js
+++ dist/api/product.js
@@ -1,21 +1,26 @@
-const express = require("express");
-const router = express.Router();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.router = void 0;
+const express_1 = __importDefault(require("express"));
+exports.router = express_1.default.Router();
 /**
  * GET product list.
  *
  * @return product list | empty.
  */
-router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
+exports.router.get("/", async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: "Get data has successfully",
+        });
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
 });
-
-module.exports = router;
+module.exports = exports.router;
